9394122d9ad0b44ab8e536d60f53e2058da6ff24
EduardoAliagaAccostupa <82846332+EduardoAliagaAccostupa@users.noreply.github.com>
2021-06-11 10:54:27 -0500

    Update Lista.h

--- ListaEnlazada/Lista.h
+++ ListaEnlazada/Lista.h
@@ -14,6 +14,7 @@ class Lista
         Lista();
         void insertar(int );
         void quitar();
+        void quitar(int);
         void mostrar();
         int mostTam();
         friend ostream& operator <<(ostream& o, const Lista& ls);
@@ -30,27 +31,96 @@ void Lista::insertar(int _valor){
     Nodo *aux=new Nodo(_valor);
     if (this->cab==NULL) { this->cab=aux;}
     else {
-        Nodo *aux2=cab;
-        while (aux2->mostSig()!=NULL){
-            aux2=aux2->mostSig();
+        if (_valor <= cab->mostValor()){
+            if (_valor == cab->mostValor()) {
+                cout << "Numero repetido!" << endl;
+                --tam;
+            }
+            else {
+                aux->darSig(cab);
+                this->cab=aux;
+            }
         }
-        aux2->darSig(aux);
+        else{
+            Nodo *aux2=cab->mostSig();
+            Nodo *aux3=cab;
+            while (aux2!=NULL && _valor > aux2->mostValor()){
+                aux2=aux2->mostSig();
+                aux3=aux3->mostSig();
+            }
+            if (aux2==NULL){ aux3->darSig(aux);}
+            else{
+                if (aux2->mostValor() == _valor){
+                    cout << "Numero repetido!" << endl;
+                    --tam;
+                }
+                else{
+                    aux3->darSig(aux);
+                    aux->darSig(aux2);
+                }
+            }
+        }
     }
     ++tam;
 }
 
 void Lista::quitar(){
-    if (cab->mostSig()==NULL){
+    if (cab==NULL) {
+        cout << "ERROR!!!" << endl;
+        ++tam;
+    }
+    else if (cab->mostSig()==NULL){
         Nodo *aux=cab;
         cab=NULL;
         delete aux;
     }
     else {
         Nodo *aux=cab, *aux2;
-        for(int i=1; i<tam; i++) { aux=aux->mostSig();}
+        for(int i=1; i<tam-1; i++) {
+            aux=aux->mostSig();
+        }
         aux2=aux->mostSig();
         delete aux2;
         aux->darSig(NULL);
+    }
+    --tam;
+}
+
+void Lista::quitar(int _valor){
+    if (cab==NULL) {
+        cout << "ERROR!!!" << endl;
+        ++tam;
+    }
+    else if (_valor <= cab->mostValor()){
+        if (_valor < cab->mostValor()){
+            cout << "Numero no existente!" << endl;
+            ++tam;
+        }
+        else {
+            Nodo *aux=cab;
+            cab=cab->mostSig();
+            delete aux;
+        }
+    }
+    else{
+        Nodo *aux=cab->mostSig();
+        Nodo *aux2=cab;
+        while (aux!=NULL && (_valor > aux->mostValor())){
+            aux=aux->mostSig();
+            aux2=aux2->mostSig();
+        }
+        if (aux==NULL){
+            cout << "Numero no existente!" << endl;
+            ++tam;
+        }
+        else if (aux->mostValor() == _valor){
+            aux2->darSig(aux->mostSig());
+            delete aux;
+        }
+        else {
+            cout << "Numero no existente!" << endl;
+            ++tam;
+        }
     }
     --tam;
 }
@@ -85,7 +155,7 @@ Lista::~Lista(){
         delete cab;
         cab=aux;
     }
-    cout << "lista borrada!" << endl;
+    cout << "Lista borrada!" << endl;
 }
 
 #endif // LISTA_H
